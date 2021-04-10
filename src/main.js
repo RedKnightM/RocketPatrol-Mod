62c77a4502fcd8e2150a247c639993396a53f6f9
Rutvik Katkoriya <rkatkori@ucsc.edu>
2021-04-10 00:05:32 -0700

    Restart#1: CommitTwo??

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-let confight = {
+let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    scene: [Menu],
 }
 
-let game = new Phaser.Game();
+let game = new Phaser.Game(config);
