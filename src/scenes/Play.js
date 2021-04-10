004b453f20ae4a934d1524b60205a439ba6d9237
Rutvik Katkoriya <rkatkori@ucsc.edu>
2021-04-10 00:39:46 -0700

    Restart#1: Starfield

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,9 +1,19 @@
 class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
+    
+    }
+
+    preload () {
+        this.load.image('starfield', 'assets/starfield.png');
     }
 
     create() {
+        
+        this.starfield = this.add.tileSprite(
+            0,0,640,480, 'starfield'
+        ).setOrigin(0,0);
+
         //green rectangle
         this.add.rectangle(
             0, 
