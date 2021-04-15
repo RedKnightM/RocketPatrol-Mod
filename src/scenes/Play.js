e1e88fb737ab9633005573ec6514f33e0b8dc17c
Rutvik Katkoriya <rkatkori@ucsc.edu>
2021-04-14 22:55:36 -0700

    Fix^7

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -130,9 +130,9 @@ class Play extends Phaser.Scene {
         scoreConfig.fixedWidth = 0;
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, '[R]estart or <- for Menu', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
-        }, null, this);
+        }, null, this); 
     }
 
     update() {
